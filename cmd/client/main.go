3e091b9ed251f78f42800140d54b23ecfe560686
hiwjd <hiwjd0@gmail.com>
2018-06-06 13:06:15 +0800

    generate clientID when left clientID arg empty

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -7,10 +7,13 @@ import (
 	"syscall"
 	"log"
 	"flag"
+	"github.com/teris-io/shortid"
+	"os/user"
+	"gopkg.in/ini.v1"
 )
 
 var (
-	clientID = flag.String("clientID", "clientID001", "")
+	clientID = flag.String("clientID", "", "specify a unique client id or left blank to auto generate one")
 	serverAddr = flag.String("serverAddr", "127.0.0.1:9992", "server addr")
 )
 
@@ -20,6 +23,38 @@ func init()  {
 
 func main() {
 	flag.Parse()
+
+	if *clientID == "" {
+		// look up from ~/.thorn file
+		usr, err := user.Current()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		iniPath := usr.HomeDir + "/.thorn"
+		if _, err := os.Stat(iniPath); os.IsNotExist(err) {
+			f, err := os.Create(iniPath)
+			if err != nil {
+				log.Fatal(err)
+			}
+			f.Close()
+		}
+
+		cfg, err := ini.Load(iniPath)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		*clientID = cfg.Section("").Key("clientID").String()
+
+		// auto generate one
+		if *clientID == "" {
+			*clientID = shortid.MustGenerate()
+			cfg.Section("").Key("clientID").SetValue(*clientID)
+			cfg.SaveTo(iniPath)
+		}
+	}
+
 	config := thorn.NewClientConfig(*clientID, *serverAddr)
 	client := thorn.NewClient(config)
 
