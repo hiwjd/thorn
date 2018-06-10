b09af8616cdd89bbb6b0b1a57971b664724de047
hiwjd <hiwjd0@gmail.com>
2018-06-10 20:54:58 +0800

    refine

--- server.go
+++ server.go
@@ -105,57 +105,6 @@ func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 			w.Write([]byte(err.Error()))
 			return
 		}
-
-		//ln, err := net.Listen("tcp", fmt.Sprintf(":%d", vport))
-		//if err != nil {
-		//	w.Write([]byte(err.Error()))
-		//	return
-		//}
-		//
-		//go func() {
-		//	// 正常情况有且只有两个连接
-		//	//   client的连接
-		//	//   使用方的连接
-		//	// 把这两个连接pipe起来
-		//	for {
-		//		conn1, err := ln.Accept()
-		//		if err != nil {
-		//			log.Println(err)
-		//		}
-		//
-		//		body := OpenPort{
-		//			Port:  int(port),
-		//			RemoteAddr: fmt.Sprintf("%s:%v", s.config.SIP(), vport),
-		//		}
-		//		bs, _ := json.Marshal(body)
-		//		p := &Packet{
-		//			Magic: MAGIC,
-		//			Version:  VERSION_1,
-		//			Reserved: uint16(0),
-		//			Cmd:      CMD_OPEN_PORT,
-		//			BodySize: uint32(len(bs)),
-		//			Body:     bs,
-		//		}
-		//
-		//		s.out <- &packetWithClientID{p, clientID}
-		//
-		//		conn2, err := ln.Accept()
-		//		if err != nil {
-		//			log.Println(err)
-		//		}
-		//
-		//		go func() {
-		//			if _, err := io.Copy(conn1, conn2); err != nil {
-		//				log.Println(err)
-		//			}
-		//		}()
-		//		go func() {
-		//			if _, err := io.Copy(conn2, conn1); err != nil {
-		//				log.Println(err)
-		//			}
-		//		}()
-		//	}
-		//}()
 	}
 }
 
